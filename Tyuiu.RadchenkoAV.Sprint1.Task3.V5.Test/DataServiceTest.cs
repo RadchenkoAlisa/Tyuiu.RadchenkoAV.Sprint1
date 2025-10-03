a16c61c975f122b7c483b3730b148b2a68b29621
alisa69 <aliska132007@gmail.ru>
2025-10-04 01:47:26 +0500

    Task3

---
+++ Tyuiu.RadchenkoAV.Sprint1.Task3.V5.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.RadchenkoAV.Sprint1.Task3.V5.Lib;
+namespace Tyuiu.RadchenkoAV.Sprint1.Task3.V5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 120;
+            double y = 3.5;
+            double wait = 420;
+            var res = ds.DistanceLength(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.RadchenkoAV.Sprint1.Task3.V5/Program.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.RadchenkoAV.Sprint1.Task3.V5.Lib;
+DataService ds = new DataService();
+Console.Title = "Спринт #1 | Выполнила: Радченко А. В. | РППб-25-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #1                                                               *");
+Console.WriteLine("* Тема: Создание решения к таску                                          *");
+Console.WriteLine("* Задание #3                                                              *");
+Console.WriteLine("* Вариант #5                                                              *");
+Console.WriteLine("* Выполнила: Радченко А. В. | РППб-25-1                                   *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("Введите расстояние между точками на карте");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите масштаб карты");
+int y = int.Parse(Console.ReadLine());
+Console.WriteLine("" + x);
+Console.WriteLine("" + y);
+
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine($"*{ds.DistanceLength(x, y)}*");
+Console.ReadLine();
