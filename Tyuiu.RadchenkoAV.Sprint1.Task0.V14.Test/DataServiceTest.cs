1902479b1b3928e09b5a087dbf99c5aaf17bd3e8
alisa69 <aliska132007@gmail.ru>
2025-10-03 22:16:10 +0500

    Add project files.

---
+++ Tyuiu.RadchenkoAV.Sprint1.Task0.V14.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.RadchenkoAV.Sprint1.Task0.V14.Lib;
+namespace Tyuiu.RadchenkoAV.Sprint1.Task0.V14.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(25, res);
+        }
+    }
+}
