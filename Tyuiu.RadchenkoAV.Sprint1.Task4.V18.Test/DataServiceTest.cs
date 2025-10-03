23990f4e8afeede3fe2c68c5395b5e2650005ff6
alisa69 <aliska132007@gmail.ru>
2025-10-04 03:17:38 +0500

    Task4

---
+++ Tyuiu.RadchenkoAV.Sprint1.Task4.V18.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.RadchenkoAV.Sprint1.Task4.V18.Lib;
+namespace Tyuiu.RadchenkoAV.Sprint1.Task4.V18.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double y = 1;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(2, res);
+        }
+    }
+}
